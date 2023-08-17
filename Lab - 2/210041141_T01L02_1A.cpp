7849b6cceb9a568ea5f1357a34789808d55d74ef
Nayeem Fardin <nfrohan110@gmail.com>
2023-08-18 04:20:51 +0600

    implement binary search in task 1 lab 2

--- Lab - 2/210041141_T01L02_1A.cpp
+++ Lab - 2/210041141_T01L02_1A.cpp
@@ -1,30 +1,44 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
-int main()
-{
-    int numberSoldier{0}, numRounds{0}, heroPower{0}, killedPowerSum{0}, numKilled{0}, powerSoldier[1024] = {0};
-    cin >> numberSoldier >> numRounds;
 
-    for (int i = 0; i < numberSoldier; i++)
-    {
-        cin >> powerSoldier[i];
-    }
-
-    
-    while (numRounds != 0)
-    {
-        cin >> heroPower;
-        numRounds--;
-
-        for (int j = 0; j < numberSoldier; j++)
-        {
-            if (heroPower >= powerSoldier[j])
-            {
-                numKilled++;
+int searchInsert(vector<int>& nums, int target) {
+        int left, right, mid;
+        left = 0;
+        right = nums.size() - 1;
+        while(left <= right){
+            mid = (right+left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
             }
         }
-        cout << numKilled << endl;
-        numKilled = 0;
-        killedPowerSum = 0;
+        return left;
+    }
+
+
+int main(){
+    int numberSoldier{0}, numRounds{0}, numKilled{0}, intp{0}, index{0};
+    vector<int> powerSoldier;
+    cin >> numberSoldier >> numRounds;
+    for(int i = 0; i < numberSoldier; i++){
+        cin >> intp;
+        powerSoldier.push_back(intp);
+    }
+    for(int j = 0; j < numRounds; j++){
+        cin >> intp;
+        index = searchInsert(powerSoldier, intp);
+        if(powerSoldier[index] == intp){
+            cout << index+1 << endl;
+        }
+        else{
+            cout << index << endl;
+        }
     }
 }
